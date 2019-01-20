35a5170206b4cff8259b53f0c6e0da4b428f7dfa
emvaladez <emely_valadez11@yahoo.com>
2019-01-20 02:42:16 -0800

    update on 1/20/19
    Update on 1/20/19

---
+++ index.js
@@ -0,0 +1,3 @@
+console.clear();
+console.log('Please feel free to message me if you would like to connect or to give me feedback on my website');
+console.log('You can also add me on LinkedIn or Github');
